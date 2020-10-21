6e65966863358476551a55ce51ea535b23edfb7e
hchiam <hchiam@users.noreply.github.com>
2020-10-20 21:23:23 -0400

    add callback ability

--- copyToClipboard.js
+++ copyToClipboard.js
@@ -1,4 +1,4 @@
-function copyToClipboard(text) {
+function copyToClipboard(text, callback) {
   try {
     var temp = document.createElement("input");
     document.body.append(temp);
@@ -6,6 +6,7 @@ function copyToClipboard(text) {
     temp.select();
     document.execCommand("copy");
     temp.remove();
+    if (callback) callback();
   } catch (err) {
     alert(
       "Could not automatically copy to clipboard. \n\n Copy this text instead: \n\n" +
